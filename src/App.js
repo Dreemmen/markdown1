c63350a3a0db9d1bd7b453ea0ff676aa04292da6
ArnVig <s-word@inbox.lv>
2023-03-21 11:09:35 +0200

    added mobx store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
+import { observer } from "mobx-react"
 import './App.css';
+import MarkdownStore from './components/markdownStore';
 
 function App() {
+
+  const sample_text = `# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)'
+  `
+  //set starting value
+  MarkdownStore.rawDataChange(sample_text)
+
   return (
     <div className="wrapper">
-      markdown
+      <textarea id="editor" onChange={(event) => MarkdownStore.rawDataChange(event.value)}>
+        {sample_text}
+      </textarea>
+      <div id="preview">
+        {MarkdownStore.rawData}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default observer(App);
